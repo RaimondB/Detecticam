1dd7514e9b536173cf3017ed8257b36230983a18
Raimond Brookman <raimondb@gmail.com>
2020-08-26 12:17:40 +0200

    feat: add healthcheck (#28)
    * Add Asp.Net stack to hosting model
    * Add heartbeat healthcheck. This will monitor that the detection loop is actually running in the backgroundservice

---
+++ DetectiCam.Core/Common/IHeartbeatReporter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DetectiCam.Core.Common
+{
+    public interface IHeartbeatReporter
+    {
+        public void ReportHeartbeat();
+    }
+}
