7414a8b2ab7a11473fed8b08329f71442b29ea3e
Raimond Brookman <raimondb@gmail.com>
2021-07-11 13:05:03 +0200

    Add snapshot integration tests

--- DetectiCam/run_tests.sh
+++ DetectiCam/run_tests.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
 
+echo "=== Start Test Run ==="
+./DetectiCam --configdir=/config &
+PROC=$!
 sleep 5
+
+echo "=== Check Snapshot ==="
+curl -o /tmp/snapshot.png -f http://localhost/api/camera/test-video/snapshot
+SNAPSHOT=$?
+
+if [ $SNAPSHOT -eq 0 ]
+then
+  echo "== Check SnapShot: OK =="
+else
+  echo "== Check SnapShot: Fail ==" >&2
+  exit 1
+fi
+
+wait $PROC
 
 FILE=""
 DIR="/captures"
 # init
 # look for empty dir 
 if [ "$(ls -A $DIR)" ]; then
+    echo "=== Check Captures : found ==="
     exit 0
 else
+    echo "=== Check Captures : not found ===" >&2
     exit 1
 fi
 # rest of the logic
